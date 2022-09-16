8d762bd704d44306a2cb23586d3408adaefa22d5
Dhruv Rajgharia <dhruvr4@illinois.edu>
2022-09-15 20:31:09 -0500

    check

--- examples/bit_map.py
+++ examples/bit_map.py
@@ -33,7 +33,7 @@ def get_map_from_distances(distance):
         ydif = np.abs(point1[1]-point2[1])
         xdif_abs = 1 if point2[0]-point1[0]>0 else -1
         ydif_abs = 1 if point2[1]-point1[1]>0 else -1
-        if(xdif*xdif+ydif*ydif > 3000):
+        if(xdif*xdif+ydif*ydif > 3500):
             i+=1
             continue
         print(point1)
