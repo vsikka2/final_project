5c231f9ea1a32f3a41cce018285a84eb8a575b66
Dhruv Rajgharia <dhruvr4@illinois.edu>
2022-09-15 19:21:32 -0500

    check

--- examples/bit_map.py
+++ examples/bit_map.py
@@ -45,11 +45,11 @@ def main():
 
     while True:
         scan_map = []
-        for i in range(-90,90,18):
+        for i in range(-90,90,1):
             scan_map.append(fc.get_distance_at(i))        
-        #print(scan_map)
-        points = get_points_from_distances(scan_map)
-        print(points)
+        print(scan_map)
+ #       points = get_points_from_distances(scan_map)
+#        print(points)
         #map = get_map_from_distances(scan_list)
 #        print(map)
     
